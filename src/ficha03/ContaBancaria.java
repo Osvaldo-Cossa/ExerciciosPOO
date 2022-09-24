ed7d00ef5f1e2b1e52075026f24aa5ef9879ea36
Osvaldo-Cossa <osvaldobernardocossa@gmail.com>
2022-09-24 13:40:36 +0200

    Resolução de Exercícios

--- src/ficha03/ContaBancaria.java
+++ src/ficha03/ContaBancaria.java
@@ -3,4 +3,8 @@ package ficha03;
 public abstract class ContaBancaria {
 	double numeroConta;
 	double saldo;
+	
+	public abstract double sacar(double saque);
+	public abstract double depositar(double deposito);
+	
 }
